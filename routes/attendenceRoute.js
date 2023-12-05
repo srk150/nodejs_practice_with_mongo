93903d5e66005bdf72400808564fda304e6e0a17
Sharukh <srkgeitpl@gmail.com>
2023-12-05 11:34:31 +0530

    remove env

--- routes/attendenceRoute.js
+++ routes/attendenceRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const attendanceController = require('../controllers/attendanceController');
 
 // Define your all attendance route
 router.post('/check-in', attendanceController.checkIn);
 router.post('/check-out', attendanceController.checkOut);
-router.get('/list', attendanceController.allAttendece);
+router.get('/list/:userId', attendanceController.allAttendece);
+
 module.exports = router;
