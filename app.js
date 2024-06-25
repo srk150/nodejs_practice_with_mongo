e9a642a4076a91199f5e2f307dcee36a968721a9
sharukh <srkgeitpl@gmail.com>
2024-06-25 20:41:50 +0530

    mongos err

--- app.js
+++ app.js
@@ -17,9 +17,35 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// MongoDB Connection
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+// // MongoDB Connection
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI);
+
+// mongoose.connection.on('connected', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// mongoose.connection.on('error', (err) => {
+//   console.error(`MongoDB connection error: ${err}`);
+// });
+
+// mongoose.connection.on('disconnected', () => {
+//   console.log('Disconnected from MongoDB');
+// });
+
+// process.on('SIGINT', async () => {
+//   await mongoose.connection.close();
+//   process.exit(0);
+// });
+
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
+  socketTimeoutMS: 45000 // Increase socket timeout to 45 seconds
+});
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
@@ -37,7 +63,6 @@ process.on('SIGINT', async () => {
   await mongoose.connection.close();
   process.exit(0);
 });
-
 
 //route files path
 const userRoutes = require('./routes/userRoutes');
@@ -67,7 +92,8 @@ app.get(baseRoute, (req, res) => {
 app.use(`${baseRoute}/user`, userRoutes);
 app.use(`${baseRoute}/attendance`, attendenceRoute);
 app.use(`${baseRoute}/task`, taskRoutes);
-app.use(`${baseRoute}/client`, clientRoutes);
+app.use(`${baseRoute}/client`, c
+  ;
 app.use(`${baseRoute}/employee`, EmpRoutes);
 app.use(`${baseRoute}/vendor`, vendorRoute);
 app.use(`${baseRoute}/reimbrushment`, reimbrushRoute);
