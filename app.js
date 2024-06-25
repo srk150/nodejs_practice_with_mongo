2052e7d47e2710bf150553f8524ecdf19e1c756c
sharukh <srkgeitpl@gmail.com>
2024-06-25 20:45:27 +0530

    mongos err

--- app.js
+++ app.js
@@ -17,28 +17,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// // MongoDB Connection
-// const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI);
-
-// mongoose.connection.on('connected', () => {
-//   console.log('Connected to MongoDB');
-// });
-
-// mongoose.connection.on('error', (err) => {
-//   console.error(`MongoDB connection error: ${err}`);
-// });
-
-// mongoose.connection.on('disconnected', () => {
-//   console.log('Disconnected from MongoDB');
-// });
-
-// process.on('SIGINT', async () => {
-//   await mongoose.connection.close();
-//   process.exit(0);
-// });
-
-
+// MongoDB Connection
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -63,6 +43,26 @@ process.on('SIGINT', async () => {
   await mongoose.connection.close();
   process.exit(0);
 });
+
+// mongoose.connect(process.env.MONGODB_URI);
+
+// mongoose.connection.on('connected', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// mongoose.connection.on('error', (err) => {
+//   console.error(`MongoDB connection error: ${err}`);
+// });
+
+// mongoose.connection.on('disconnected', () => {
+//   console.log('Disconnected from MongoDB');
+// });
+
+// process.on('SIGINT', async () => {
+//   await mongoose.connection.close();
+//   process.exit(0);
+// });
+
 
 //route files path
 const userRoutes = require('./routes/userRoutes');
@@ -92,8 +92,7 @@ app.get(baseRoute, (req, res) => {
 app.use(`${baseRoute}/user`, userRoutes);
 app.use(`${baseRoute}/attendance`, attendenceRoute);
 app.use(`${baseRoute}/task`, taskRoutes);
-app.use(`${baseRoute}/client`, c
-  ;
+app.use(`${baseRoute}/client`, clientRoutes);
 app.use(`${baseRoute}/employee`, EmpRoutes);
 app.use(`${baseRoute}/vendor`, vendorRoute);
 app.use(`${baseRoute}/reimbrushment`, reimbrushRoute);
